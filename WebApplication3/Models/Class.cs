f040c2cfb602481a43e66fa094a849a3f525e33e
George Suvorov <tudziknova@gmail.com>
2024-11-13 20:22:25 +0300

    Добавьте файлы проекта.

---
+++ WebApplication3/Models/Class.cs
@@ -0,0 +1,10 @@
+﻿namespace BookApp.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }           // ID книги
+        public string Title { get; set; }     // Название книги
+        public string Author { get; set; }    // Автор книги
+    }
+}
+
