ff066c90bd2b789ad6fe423b884da718c3445997
George Suvorov <tudziknova@gmail.com>
2024-11-14 14:04:35 +0300

    Описание изменений

--- WebApplication3/Program.cs
+++ WebApplication3/Program.cs
@@ -3,14 +3,15 @@ using BookApp.Data;
 using System;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseUrls("http://localhost:44301"); // Замените на нужный порт
+
 
 // Настраиваем DbContext с использованием SQLite
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<AppDbContext>(options =>options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
-
 var app = builder.Build();
+app.UseHttpsRedirection(); // Перенаправление на HTTPS
 app.MapControllers();
 app.Run();
 app.UseDefaultFiles();       // Включает использование index.html по умолчанию
